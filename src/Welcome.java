c3d175c24bc0aace2cc0581ceae95e3668b00191
Sania2001 <saniagshinde@gmail.com>
2022-03-14 17:16:50 +0530

    Beginner

---
+++ src/Welcome.java
@@ -0,0 +1,10 @@
+public class Welcome {
+
+    public static void main(String[] args) {
+//        String name = "Sania";
+//        System.out.println("Hello " + name);
+        Integer a=5, b=-3, c;
+        c=a+b;
+        System.out.println(c);
+    }
+}
